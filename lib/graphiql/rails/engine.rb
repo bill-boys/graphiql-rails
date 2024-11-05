1dd59c763fde0626e1cd767c219379f54528113e
Hafidz Mahrus <havus.it@gmail.com>
2024-11-05 16:59:19 +0700

    remove style

--- lib/graphiql/rails/engine.rb
+++ lib/graphiql/rails/engine.rb
@@ -3,14 +3,14 @@ module GraphiQL
     class Engine < ::Rails::Engine
       isolate_namespace GraphiQL::Rails
 
-      if defined?(Sprockets) && Sprockets::VERSION.chr.to_i >= 4
-        initializer 'graphiql.assets.precompile' do |app|
-          app.config.assets.precompile += %w(
-            graphiql/rails/application.css
-            graphiql/rails/application.js
-          )
-        end
-      end
+      # if defined?(Sprockets) && Sprockets::VERSION.chr.to_i >= 4
+      #   initializer 'graphiql.assets.precompile' do |app|
+      #     app.config.assets.precompile += %w(
+      #       graphiql/rails/application.css
+      #       graphiql/rails/application.js
+      #     )
+      #   end
+      # end
     end
   end
 end
